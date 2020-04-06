e277c4849cb0b66614ae6422b757ddb1ffb5a9ba
pirangabaneto <pirangabaneto@gmail.com>
2020-04-06 01:10:23 -0300

    creating PizzaController

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizzas', function () {
-  //get data from db
-    $pizzas = [
-      ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-      ['type' => 'volcano', 'base' => 'garlic crust'],
-      ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
-    return view('pizzas', ['pizzas' => $pizzas]);
-});
+Route::get('/pizzas', 'PizzaController@index');
+
+Route::get('/pizzas/{id}', 'PizzaController@show');
